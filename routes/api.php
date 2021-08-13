af7eac2f008527907633df42aa99b5cdff5bc4bf
mataa-hewa-leroy <mataa.abiguime17@gmail.com>
2021-08-13 01:25:11 +0000

    Passport Authentification controller  done

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use PassportAuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,13 +14,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
-Route::post('register',[passportAuthController::class,'register']);
-Route::post('login',[passportAuthController::class,'login']);
+Route::post('register',[PassportAuthController::class,'register']);
+Route::post('login',[PassportAuthController::class,'login']);
 //add this middleware to ensure that every request is authenticated
 Route::middleware('auth:api')->group(function(){
-    Route::get('user', [passportAuthController::class,'authenticatedUserDetails']);
+    Route::get('user', [PassportAuthController::class,'authenticatedUserDetails']);
 });
+
+
+Route::group([
+    'prefix' => 'v1',
+    'as' => 'api.',
+    'middleware' => ['auth:api']
+], function () {
+    //lists all users
+    // Route::post('/all-user', 'API\ V1\ApiController@allUsers')->name('all-user');*
+    Route::post('register',[PassportAuthController::class,'register']);
+Route::post('login',[PassportAuthController::class,'login']);
+});
+
+// //auth routes
+// Route::post('v1/user-register', 'API\ V1\AuthController@register');
+// Route::post('v1/user-login', 'API\ V1\AuthController@login');
+
+// //lists all active tests
+// Route::post('v1/test', 'API\ V1\ApiController@getActiveTest');
+
+// //lists payment detail of an individual
+// Route::post('v1/paymentdetail/{id}', 'API\ V1\API@paymentDetail');
+
+// //lists test user details
+// Route::post('v1/testuser', 'API\ V1\ApiController@testUsers');
+
+// //lists all active packages
+// Route::post('v1/package', 'API\ V1\ApiController@getPackages');
+
+// //lists every tests within the package
+// Route::post('v1/packagedetail/{id}', 'API\ V1\ApiController@getPackageDetails');
+
+// //lists all test details(test sections, questions, options)
+// Route::post('v1/testdetail/{id}', 'API\ V1\ApiController@testDetails');
